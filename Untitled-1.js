d733193bfde86ec327df44b7fbe323562471caf1
saiyedsalim08 <101225025+saiyedsalim08@users.noreply.github.com>
2022-09-05 19:09:47 -0400

    add arrow exercise es2015 to github

---
+++ Untitled-1.js
@@ -0,0 +1,25 @@
+// ES5 Map Callback
+function double(arr) {
+  return arr.map(function(val) {
+    return val * 2;
+  });
+}
+
+/* Write an ES2015 Version */
+
+const double = arr => arr.map(val => val * 2);
+
+//Refactor the following function to use arrow functions:
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
+
+  /* Write an ES2015 Version */
+
+  const squareAndFindEvens = numbers => numbers.map(val => val ** 2).filter(square => square % 2 === 0)
